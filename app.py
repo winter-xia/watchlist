161a431ebec8f5e1b79e448eb990adb782646d4b
Sofia Xia <sofiax@foxmail.com>
2022-05-02 22:35:14 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import click
-from flask import Flask, escape, url_for, render_template
+from flask import Flask, escape, url_for, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
 WIN = sys.platform.startswith('win')
@@ -14,6 +14,7 @@ else:  # 否则使用四个斜线
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config["SECRET_KEY"] = 'secret123'
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
@@ -99,12 +100,60 @@ def inject_user():  # 函数名可以随意修改
 
 
 # 返回渲染好的模板作为响应
-@app.route('/sofia')
+@app.route('/sofia', methods=['GET', 'POST'])
 def index():
+	if request.method == 'POST':
+		# 获取表单数据
+		title = request.form.get('title')  # 传入表单对应输入字段的name值
+		year = request.form.get('year')
+		# 验证数据
+		if not title or not year or len(year) > 4 or len(title) > 60:
+			flash('Invalid input.')  # 显示错误提示
+			return redirect(url_for('index'))  # 重定向回主页
+		# 保存表单数据到数据库
+		movie = Movie(title=title, year=year) # 创建记录
+		db.session.add(movie)  # 添加到数据库会话
+		db.session.commit()  # 提交数据库会话
+		flash('Item created.')  # 显示成功创建的提示
+		return redirect(url_for('index'))  # 重定向回主页
+			
 	# user = User.query.first() # 读取用户记录
 	movies = Movie.query.all()  # 读取所有电影记录
 	# return render_template('index.html', user=user, movies=movies)
 	return render_template('index.html', movies=movies)
+
+
+#  编辑电影条目
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	
+	if request.method == 'POST':  # 处理编辑表单的提交请求
+		title = request.form['title']
+		year = request.form['year']
+		
+		if not title or not year or len(year) > 4 or len(title) > 60:
+			flash('Invalid input.')
+			return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+		
+		movie.title = title
+		movie.year = year
+		db.session.commit()
+		flash('Item updated.')
+		return redirect(url_for('index'))
+	
+	# print("test3ee3r3grthrthtrnj")
+	return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+
+#  删除电影条目
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	db.session.delete(movie)
+	db.session.commit()
+	flash('Item deleted.')
+	return redirect(url_for('index'))
 
 
 @app.errorhandler(404)  # 传入要处理的错误代码
@@ -112,5 +161,4 @@ def page_not_found(e):  # 接受异常对象作为参数
 	# user = User.query.first()
 	# return render_template('404.html', user=user), 404  # 返回模版和状态码
 	return render_template('404.html'), 404  # 返回模版和状态码
-
 
